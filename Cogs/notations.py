5adeb44ffc9cd90982c7a5f7e0859c2515e01169
maresyp <mattq.2000@icloud.com>
2021-04-19 21:14:20 +0200

    notations -> theta_check

--- Cogs/notations.py
+++ Cogs/notations.py
@@ -13,8 +13,8 @@ class Notations(commands.Cog):
         help=f'Usage: !theta 2*n**2 - 3*n + 2\n'
              f'Usage: !tt n**2 - 2*n\n'
              f'Or:\n'
-             f'Usage: !theta 2*n^2 - 3^n + 2\n'
-             f'Usage: !tt n^2 - 2^n\n'
+             f'Usage: !theta 2*n^2 - 3*n + 2\n'
+             f'Usage: !tt n^2 - 2*n\n'
     )
     async def theta(self, ctx, *args):
         user_input: str = ''.join(args)
@@ -42,8 +42,23 @@ class Notations(commands.Cog):
         help=f'Check if c1, c2, n0 are correct for given equation\n'
              f'Usage: !theta_check'
     )
-    async def theta_check(self, ctx, *args):
-        pass
+    async def theta_check(self, ctx, c1: str, c2: str, n0: int, *args):
+        user_input: str = ''.join(args)
+        user_input = user_input.replace('^', '**')  # just in case someone used ^ power sign instead of **
+        print(f'c1 = {c1}, c2 = {c2}, n0 = {n0}, rest = {user_input}\n')  # TODO: DEBUG
+        n = sympy.symbols('n')
+        simplified = sympy.simplify(sympy.parsing.parse_expr(f'({user_input}) / n**2'))
+        c2_check = sympy.limit_seq(simplified, n)
+        n0_check = int(n0) >= 1 and sympy.simplify(f"{str(simplified).replace('n', f'{n0}')} > 0")
+        c1_check = sympy.simplify(f"{str(simplified).replace('n', f'{n0}')}")
+        print(f'c1 = {str(c1_check) == c1}, c2 = {c2_check == c2}, n0 = {n0_check}')  # TODO: DEBUG
+
+        result: str = f'{user_input}\n' \
+                      f"c1 = {c1} -> {'Correct' if str(c1_check) == c1 else f'Wrong, should be {c1_check}'}\n" \
+                      f"c2 = {c2} -> {'Correct' if str(c2_check) == c2 else f'Wrong, should be {c2_check}'}\n" \
+                      f"n0 = {n0} -> {'Correct' if n0_check else 'Wrong'}\n"
+
+        await ctx.reply(make_reply(ctx, result))
 
     async def cog_command_error(self, ctx, error):
         print(error)
