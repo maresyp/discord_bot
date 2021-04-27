d1675b83787ab10c2260aaf12fcfe5ac82e0d428
maresyp <mattq.2000@icloud.com>
2021-04-27 22:39:28 +0200

    add min/max constants

--- Cogs/Cog_sorting.py
+++ Cogs/Cog_sorting.py
@@ -6,6 +6,9 @@ from utils.sorting import bubble_sort, insert_sort, select_sort, pivot_first, pi
 from utils.utils import make_reply
 from typing import Union
 
+MIN_ELEMENTS: int = 2
+MAX_ELEMENTS: int = 100
+
 
 class Sorting(commands.Cog):
 
@@ -18,7 +21,7 @@ class Sorting(commands.Cog):
     async def bubble(self, ctx, *args: Union[int, float]):
 
         # run in separate thread for non blocking io
-        result: tuple[list, str] = await asyncio.to_thread(bubble_sort, list(args), 2, 100)
+        result: tuple[list, str] = await asyncio.to_thread(bubble_sort, list(args), MIN_ELEMENTS, MAX_ELEMENTS)
         await ctx.reply(make_reply(ctx, result[1]))
 
     @commands.command(
@@ -30,7 +33,7 @@ class Sorting(commands.Cog):
     async def insert(self, ctx, *args: Union[int, float]):
 
         # run in separate thread for non blocking io
-        result: tuple[list, str] = await asyncio.to_thread(insert_sort, list(args), 2, 100)
+        result: tuple[list, str] = await asyncio.to_thread(insert_sort, list(args), MIN_ELEMENTS, MAX_ELEMENTS)
         await ctx.reply(make_reply(ctx, result[1]))
 
     @commands.command(
@@ -42,7 +45,7 @@ class Sorting(commands.Cog):
     async def select(self, ctx, *args: Union[int, float]):
 
         # run in separate thread for non blocking io
-        result: tuple[list, str] = await asyncio.to_thread(select_sort, list(args), 2, 100)
+        result: tuple[list, str] = await asyncio.to_thread(select_sort, list(args), MIN_ELEMENTS, MAX_ELEMENTS)
         await ctx.reply(make_reply(ctx, result[1]))
 
     @commands.command(
@@ -53,7 +56,7 @@ class Sorting(commands.Cog):
     )
     async def pv_first(self, ctx, *args: Union[int, float]):
 
-        result: list = await asyncio.to_thread(pivot_first, list(args), 2, 100)
+        result: list = await asyncio.to_thread(pivot_first, list(args), MIN_ELEMENTS, MAX_ELEMENTS)
         result: str = ' '.join((str(elem) for elem in result))
         await ctx.reply(make_reply(ctx, result))
 
@@ -65,7 +68,7 @@ class Sorting(commands.Cog):
     )
     async def pv_last(self, ctx, *args: Union[int, float]):
 
-        result: list = await asyncio.to_thread(pivot_last, list(args), 2, 100)
+        result: list = await asyncio.to_thread(pivot_last, list(args), MIN_ELEMENTS, MAX_ELEMENTS)
         result: str = ' '.join((str(elem) for elem in result))
         await ctx.reply(make_reply(ctx, result))
 
