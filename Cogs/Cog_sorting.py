fbf0365543404724973aa6380eb0db53619f1861
maresyp <mattq.2000@icloud.com>
2021-04-27 21:58:43 +0200

    update

--- Cogs/Cog_sorting.py
+++ Cogs/Cog_sorting.py
@@ -6,6 +6,7 @@ from utils.sorting import bubble_sort, insert_sort, select_sort, pivot_first, pi
 from utils.utils import make_reply, check_for_elements
 from typing import Union
 
+
 class Sorting(commands.Cog):
 
     @commands.command(
@@ -14,10 +15,10 @@ class Sorting(commands.Cog):
         brief='Bubble Sort',
         help='Usage: !bubble 1 2 3 <- All elements are separated by space'
     )
-    async def bubble(self, ctx, *args: Union[int, float, complex]):
+    async def bubble(self, ctx, *args: Union[int, float]):
 
         # run in separate thread for non blocking io
-        result: tuple[list, str] = await asyncio.to_thread(bubble_sort, args, 2, 100)
+        result: tuple[list, str] = await asyncio.to_thread(bubble_sort, list(args), 2, 100)
         await ctx.reply(make_reply(ctx, result[1]))
 
     @commands.command(
@@ -26,14 +27,10 @@ class Sorting(commands.Cog):
         aliases=['is'],
         help='Usage: !insert 1 2 3 <- All elements are separated by space'
     )
-    async def insert(self, ctx, *args):
-
-        __data: list = []
-        for elem in args:
-            __data.append(int(elem))
+    async def insert(self, ctx, *args: Union[int, float]):
 
         # run in separate thread for non blocking io
-        result: tuple[list, str] = await asyncio.to_thread(insert_sort, __data, 2, 100)
+        result: tuple[list, str] = await asyncio.to_thread(insert_sort, list(args), 2, 100)
         await ctx.reply(make_reply(ctx, result[1]))
 
     @commands.command(
@@ -42,14 +39,10 @@ class Sorting(commands.Cog):
         aliases=['ss'],
         help='Usage: !select 1 2 3 <- All elements are separated by space'
     )
-    async def select(self, ctx, *args):
-
-        __data: list = []
-        for elem in args:
-            __data.append(int(elem))
+    async def select(self, ctx, *args: Union[int, float]):
 
         # run in separate thread for non blocking io
-        result: tuple[list, str] = await asyncio.to_thread(select_sort, __data, 2, 100)
+        result: tuple[list, str] = await asyncio.to_thread(select_sort, list(args), 2, 100)
         await ctx.reply(make_reply(ctx, result[1]))
 
     @commands.command(
@@ -58,13 +51,9 @@ class Sorting(commands.Cog):
         aliases=['pf'],
         help='Usage: !pivot_first 1 2 3 <- All elements are separated by space'
     )
-    async def pv_first(self, ctx, *args):
+    async def pv_first(self, ctx, *args: Union[int, float]):
 
-        __data: list = []
-        for elem in args:
-            __data.append(int(elem))
-
-        result: list = await asyncio.to_thread(pivot_first, __data, 2, 100)
+        result: list = await asyncio.to_thread(pivot_first, list(args), 2, 100)
         result: str = ' '.join((str(elem) for elem in result))
         await ctx.reply(make_reply(ctx, result))
 
@@ -74,18 +63,14 @@ class Sorting(commands.Cog):
         aliases=['pl'],
         help='Usage: !pivot_last 1 2 3 <- All elements are separated by space'
     )
-    async def pv_last(self, ctx, *args):
+    async def pv_last(self, ctx, *args: Union[int, float]):
 
-        __data: list = []
-        for elem in args:
-            __data.append(int(elem))
-
-        result: list = await asyncio.to_thread(pivot_last, __data, 2, 100)
+        result: list = await asyncio.to_thread(pivot_last, list(args), 2, 100)
         result: str = ' '.join((str(elem) for elem in result))
         await ctx.reply(make_reply(ctx, result))
 
     async def cog_command_error(self, ctx, error):
-        print('debug -> ', error)
+        print('debug -> ', error, type(error))
         if hasattr(error, 'original'):  # Handle other Exceptions
 
             if isinstance(error.original, ValueError):
@@ -96,5 +81,9 @@ class Sorting(commands.Cog):
         else:  # Handle errors that inherit from CommandError
 
             if isinstance(error, commands.errors.MissingRequiredArgument):
+                await ctx.reply(f'> {error}\n'
+                                f'> Please check usage with !help command and try again')
+            # Converter failed on all types from Union
+            if isinstance(error, commands.errors.BadUnionArgument):
                 await ctx.reply(f'> {error}\n'
                                 f'> Please check usage with !help command and try again')
