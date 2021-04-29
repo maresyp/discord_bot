e70beadaa707df1bc5bf378ad498de2ac4325920
maresyp <mattq.2000@icloud.com>
2021-04-29 10:28:42 +0200

    add basic cases

--- utils/utils_tests/test_functions.py
+++ utils/utils_tests/test_functions.py
@@ -1,4 +1,25 @@
 import unittest
 
+from utils.functions import sort_functions
+
+
+class TestFunctionSort(unittest.TestCase):
+    def test_basic_cases(self):
+        self.assertListEqual(
+            ['1', 'log(log(n))', 'log(n)**3', 'n*log(n)', 'n**(3/2)'],
+            sort_functions(['n*log(n)', 'log(log(n))', 'log(n)**3', '1', 'n**(3/2)'], 2, 10)
+        )
+
+        self.assertListEqual(
+            ['1', 'log(n)**3', 'n**(9/10)', 'n*log(n)', 'factorial(n)'],
+            sort_functions(['n*log(n)', 'factorial(n)', 'log(n)**3', '1', 'n**(9/10)'], 2, 10)
+        )
+
+        self.assertListEqual(
+            ['1', 'log(log(n))', 'log(n)**3', 'n**(3/2)', 'n**2'],
+            sort_functions(['log(log(n))', '1', 'log(n)**3', 'n**2', 'n**(3/2)'], 2, 10)
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
