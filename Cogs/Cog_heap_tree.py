02e9c4d93407084ad72dc578e4827c1d2ece6b36
maresyp <mattq.2000@icloud.com>
2021-05-09 22:36:01 +0200

    change method names

--- Cogs/Cog_heap_tree.py
+++ Cogs/Cog_heap_tree.py
@@ -20,7 +20,7 @@ class HeapTrees(commands.Cog):
         brief='Heap Min',
         help='Usage: !heap_build_min 1 2 3 4 5'
     )
-    async def build_max(self, ctx, *args: int):
+    async def build_min(self, ctx, *args: int):
         pass
 
     @commands.command(
@@ -29,5 +29,5 @@ class HeapTrees(commands.Cog):
         brief='Heap',
         help='Usage: !heap_build 1 2 3 4 5'
     )
-    async def build_max(self, ctx, *args: int):
+    async def build_x(self, ctx, *args: int):
         pass
