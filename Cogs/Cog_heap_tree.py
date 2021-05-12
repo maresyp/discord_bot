871c0a882932882c199876a0b223e9e94faf5bb5
maresyp <mattq.2000@icloud.com>
2021-05-12 21:16:12 +0200

    reverse

--- Cogs/Cog_heap_tree.py
+++ Cogs/Cog_heap_tree.py
@@ -1,8 +1,5 @@
-import binarytree
-import discord
 from discord.ext import commands
 import asyncio
-from typing import Optional
 
 from utils.heap_tree import build_max_heap, build_min_heap, build_heap, prepare_response
 from utils.utils import make_reply
@@ -17,8 +14,7 @@ class HeapTrees(commands.Cog):
         help='Build Max Heap Tree\n'
              'Usage: !heap_build_max 1 2 3 4 5'
     )
-    async def build_max(self, ctx, *args: Optional[int]):
-
+    async def build_max(self, ctx, *args: int):
         response: str = await asyncio.to_thread(prepare_response, list(args), build_max_heap)
         await ctx.reply(make_reply(ctx, response))
 
@@ -29,7 +25,7 @@ class HeapTrees(commands.Cog):
         help='Build Min Heap Tree\n'
              'Usage: !heap_build_min 1 2 3 4 5'
     )
-    async def build_min(self, ctx, *args: Optional[int]):
+    async def build_min(self, ctx, *args: int):
         response: str = await asyncio.to_thread(prepare_response, list(args), build_min_heap)
         await ctx.reply(make_reply(ctx, response))
 
@@ -40,9 +36,10 @@ class HeapTrees(commands.Cog):
         help='Build Heap Tree\n'
              'Usage: !heap_build 1 2 3 4 5'
     )
-    async def build_x(self, ctx, *args: Optional[int]):
+    async def build_x(self, ctx, *args: int):
         response: str = await asyncio.to_thread(prepare_response, list(args), build_heap)
         await ctx.reply(make_reply(ctx, response))
 
     async def cog_command_error(self, ctx, error):
-        await ctx.reply(f'{error}')
+        print(error)
+        # await ctx.reply(f'{error}')
