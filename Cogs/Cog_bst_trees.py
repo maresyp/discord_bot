398698b0ed1ff1e38e8ee360dadfb99b10b6816d
maresyp <mattq.2000@icloud.com>
2021-05-12 22:10:54 +0200

    add help

--- Cogs/Cog_bst_trees.py
+++ Cogs/Cog_bst_trees.py
@@ -11,7 +11,20 @@ class BSTTrees(commands.Cog):
         name='bst_build',
         aliases=['bstb'],
         brief='Build BST Tree',
-        help='Help coming soon...'
+        help=f'With this command you can build bst tree\n'
+             f'For example:\n'
+             f"""
+                   ___15______
+                  /           \\
+                 7         ____30
+                / \\       /      \\
+               4   13    25       34
+              /            \\
+             2              27
+             \n"""
+             f'For this output, input should be:\n'
+             f'!bst_build 15 7 30 4 13 25 34 2 None None None None 27 None None\n'
+             f'In general None means that there is no child at this index'
     )
     async def build_bst(self, ctx, *args):
         def prepare(arr: list[str]) -> str:
@@ -21,5 +34,6 @@ class BSTTrees(commands.Cog):
                    f"Preorder: {' '.join(str(elem.value) for elem in bst_tree.preorder)}\n" \
                    f"Postorder: {' '.join(str(elem.value) for elem in bst_tree.postorder)}\n" \
                    f'{bst_tree.__str__()}'
+
         response: str = await asyncio.to_thread(prepare, list(args))
         await ctx.reply(make_reply(ctx, response))
